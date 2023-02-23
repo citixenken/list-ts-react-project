cbdc58a02de28c413f459acf7964c1fb3fe316a1
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-23 17:24:50 +0300

    useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,10 @@
-import { useState, useEffect, useCallback, useMemo, useRef } from "react";
-
-interface User {
-  id: number;
-  username: string;
-}
-function App() {
-  const [count, setCount] = useState(0);
-  const [users, setUsers] = useState<User[] | null>(null);
-
-  // useRef
-  const inputRef = useRef<HTMLInputElement>(null);
-  console.log("Current value", inputRef?.current);
-  console.log("Current value", inputRef?.current?.value);
-
-  useEffect(() => {
-    console.log("mounting");
-    console.log("Users: ", users);
-
-    return () => console.log("unmounting");
-  }, [users]);
-
-  // useCallback => memoize a function
-  const addThree = useCallback(() => {
-    setCount((prev) => prev + 3);
-  }, []);
-
-  // useMemo => memoize a value
-  type fibFn = (n: number) => number;
-
-  const fib: fibFn = (n) => {
-    if (n < 2) return n;
-    return fib(n - 1) + fib(n - 2);
-  };
-
-  const myNum: number = 12;
-
-  const fibResult = useMemo(() => fib(myNum), [myNum]);
-
+import Counter from "./Counter";
+const App = () => {
   return (
-    <>
-      <input type="text" ref={inputRef} />
-      <p>Count is {count}</p>
-      <button onClick={addThree}>+</button>
-      <p>{fibResult}</p>
-    </>
+    <div>
+      <Counter>{(num: number) => <>Current count: {num}</>}</Counter>
+    </div>
   );
-}
+};
 
 export default App;
