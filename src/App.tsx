568276ac5682152b89c5f2193c19b4b45c75329a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-24 19:09:49 +0300

    feat: scaffolding base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState } from "react";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  const [viewCart, setViewCart] = useState<boolean>(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
+    </>
+  );
+
+  return content;
 };
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+type PropsType = {
+  viewCart: boolean;
+};
+const Footer = ({ viewCart }: PropsType) => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
